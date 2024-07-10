5a87c159aef3dd476d4a32e47f9519371249ff1d
Arham Abeer <127412341+arhamabeer-braincrop@users.noreply.github.com>
2024-07-10 11:38:11 +0500

    Update BookingDatesForm log

--- src/forms/BookingDatesForm/BookingDatesForm.js
+++ src/forms/BookingDatesForm/BookingDatesForm.js
@@ -174,7 +174,7 @@ export class BookingDatesFormComponent extends Component {
 
           const showEstimatedBreakdown =
             bookingData && lineItems && !fetchLineItemsInProgress && !fetchLineItemsError;
-
+console.log('EstimatedBreakdownMaybe', promocode, bookingData, lineItems, bookingType);
           const bookingInfoMaybe = showEstimatedBreakdown ? (
             <div className={css.priceBreakdownContainer}>
               <h3 className={css.priceBreakdownTitle}>
